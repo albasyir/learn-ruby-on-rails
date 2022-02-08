8296c2f742d35bae2e0ba24dabf04d1e076af24f
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2022-02-08 13:53:57 +0700

    init project

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root 'articles#index'
+
+  # get '/articles', to: 'articles#index'
+  # get '/articles/:id', to: 'articles#show'
+
+  resources :articles
+end
