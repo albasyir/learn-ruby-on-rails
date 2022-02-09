9eda22352337629029667de26e8e8c2ec03076be
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2022-02-10 00:05:02 +0700

    add comment

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   # get '/articles', to: 'articles#index'
   # get '/articles/:id', to: 'articles#show'
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
 end
